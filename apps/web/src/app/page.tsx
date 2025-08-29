538e4d9148c19bb87a09c8e89ca1fb92c0064544
YakshitAgarwal <yakshitagarwal05@gmail.com>
2025-08-29 19:38:55 +0530

    Changes made

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Footer } from '@/components/landing/footer'
 import { Header } from '@/components/landing/header'
+import Hero from '@/components/landing/hero'
 
 export default function Home() {
 	return (
 		<main>
 			<Header />
+			<Hero />
 			<Footer />
 		</main>
 	)
