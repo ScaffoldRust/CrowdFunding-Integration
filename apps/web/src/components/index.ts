7418f209f8a85b899ba129fdc0d8126ff7196836
Sandijigs <idjigheresandra@gmail.com>
2025-09-28 01:04:38 +0100

    feat: added feature cards

--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -3,3 +3,6 @@ export { CampaignCard, CampaignCardSkeleton } from './campaign-card'
 
 export type { StatCardProps } from './ui/stat-card'
 export { StatCard } from './ui/stat-card'
+
+export type { FeatureCardProps } from './ui/feature-card'
+export { FeatureCard } from './ui/feature-card'
